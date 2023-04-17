240725694a8bc651ef2c048386546a3a0e09ff3e
GoodwinSoftware <GoodwinSoftware@users.noreply.github.com>
2023-04-17 15:51:43 +0100

    Update StartUnits.sql
    Increased deity settler count to 500,000 given 50 of each other type

--- StartUnits.sql
+++ StartUnits.sql
@@ -2,8 +2,8 @@ INSERT INTO MajorStartingUnits (Unit, Era, Quantity, NotStartTile, AiOnly, MinDi
 -- this is reversed because the system is designed to be additive as difficulties go up.
 -- i.e., any units you give to Settler will also be given to all the other difficulties.
 -- deity is the "easiest" setting, and gives the most units (and the better yields in the other file)
-("UNIT_SCOUT", "ERA_ANCIENT", 1, 1, 0, "DIFFICULTY_IMMORTAL", 0),
+("UNIT_SCOUT", "ERA_ANCIENT", 50, 1, 0, "DIFFICULTY_IMMORTAL", 0),
 
-("UNIT_SETTLER", "ERA_ANCIENT", 1, 1, 0, "DIFFICULTY_DEITY", 0),
-("UNIT_BUILDER", "ERA_ANCIENT", 1, 1, 0, "DIFFICULTY_DEITY", 0),
-("UNIT_SCOUT", "ERA_ANCIENT", 1, 1, 0, "DIFFICULTY_DEITY", 0);
+("UNIT_SETTLER", "ERA_ANCIENT", 500000, 1, 0, "DIFFICULTY_DEITY", 0),
+("UNIT_BUILDER", "ERA_ANCIENT", 50, 1, 0, "DIFFICULTY_DEITY", 0),
+("UNIT_SCOUT", "ERA_ANCIENT", 50, 1, 0, "DIFFICULTY_DEITY", 0);
